2daeab834060d9e84c21a48907342df3e9d73c0f
asipple1 <asipple1@gmail.com>
2025-02-26 16:28:56 -1000

    inital-commit

---
+++ src/data/navigation.js
@@ -0,0 +1,14 @@
+export const mainNavigation = [
+  { name: "About", path: "/about" },
+  { name: "Services", path: "/services" },
+  // {
+  //   name: "Listings",
+  //   isDropdown: true,
+  //   items: [
+  //     { name: "Residential", path: "/listings/residential" },
+  //     { name: "Commercial", path: "/listings/commercial" },
+  //   ],
+  // },
+  { name: "Blog", path: "/blog" },
+  { name: "Contact", path: "/contact" },
+];
